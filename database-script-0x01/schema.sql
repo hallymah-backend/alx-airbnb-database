4d36c8d8af204e64ed0b81680c1a32948cc22e0a
hallymah-backend <olakitanhalimat@gmail.com>
2026-01-07 17:34:05 +0100

    done

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,16 +1,16 @@
 -- USER TABLE
--- CREATE TABLE users (
---     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     first_name VARCHAR(50) NOT NULL,
---     last_name VARCHAR(50) NOT NULL,
---     email VARCHAR(150) UNIQUE NOT NULL,
---     password_hash VARCHAR(25) NOT NULL,
---     phone_number VARCHAR(20),
---     role VARCHAR(10) CHECK (role IN ('guest', 'host', 'admin')) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password_hash VARCHAR(25) NOT NULL,
+    phone_number VARCHAR(20),
+    role VARCHAR(10) CHECK (role IN ('guest', 'host', 'admin')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- -- PROPERTY TABLE
+- -- PROPERTY TABLE
 CREATE TABLE Property (
     property_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     host_id UUID NOT NULL,
@@ -23,19 +23,19 @@ CREATE TABLE Property (
     FOREIGN KEY (host_id) REFERENCES users(user_id)
 );
 
--- CREATE INDEX idx_property_host_id ON Property(host_id);
--- CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX idx_property_host_id ON Property(host_id);
+CREATE INDEX idx_property_location ON Property(location);
 
 -- -- BOOKING TABLE
--- CREATE TABLE Booking (
---     booking_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     property_id UUID NOT NULL,
---     user_id UUID NOT NULL,
---     start_date DATE NOT NULL,
---     end_date DATE NOT NULL,
---     total_price DECIMAL(10, 2) NOT NULL,
---     status VARCHAR(20) CHECK (status IN ('pending', 'confirmed', 'canceled')) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE Booking (
+    booking_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    property_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(20) CHECK (status IN ('pending', 'confirmed', 'canceled')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
 --     FOREIGN KEY (property_id) REFERENCES Property(property_id),
 --     FOREIGN KEY (user_id) REFERENCES users(user_id)
