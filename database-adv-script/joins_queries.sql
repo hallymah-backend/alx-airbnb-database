b1b2713bc2252a1c6c9cef7e7326da77c4fb4db2
hallymah-backend <olakitanhalimat@gmail.com>
2026-01-16 05:52:18 +0100

    inner joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,6 @@
+SELECT users.user_id, users.email
+   FROM users 
+   INNER JOIN Property
+   ON users.user_id = property.host_id;
+--    WHERE LOWER(last_name) = LOWER('olakitan')
+-- ORDER BY  email DESC;
