070e3565b8ef5bacdd50e259708f9437a5af47e7
hallymah-backend <olakitanhalimat@gmail.com>
2026-01-16 17:33:48 +0100

    jchv

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -6,3 +6,13 @@ LEFT JOIN booking b
     ON u.user_id = b.user_id
 GROUP BY u.user_id
 ORDER BY total_bookings DESC;
+
+
+SELECT
+    p.property_id,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM property p
+LEFT JOIN booking b
+    ON p.property_id = b.property_id
+GROUP BY p.property_id;
