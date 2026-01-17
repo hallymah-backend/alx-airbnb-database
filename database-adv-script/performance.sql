9dc7408ae61ac1159e6dd18913de391bacc9d22f
hallymah-backend <olakitanhalimat@gmail.com>
2026-01-17 17:16:56 +0100

    perform

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,20 +1,26 @@
--- performance.sql
+-- perfomance.sql
 
--- Step 1: Analyze the query before optimization
-EXPLAIN ANALYZE
+WITH booking AS (
+SELECT 
+    booking.booking_id,
+    booking.start_date,
+    booking.end_date,
+    users.user_id,
+    users.first_name || ' ' || users.last_name AS user_name,
+    users.email AS user_email,
+    property.property_id,
+    property.name AS property_name,
+    property.location AS property_location
+   
+FROM booking booking
+JOIN users users ON booking.user_id = users.user_id
+JOIN property property ON booking.property_id = property.property_id
+)
 SELECT
-    b.booking_id,
-    u.user_id,
-    u.first_name || ' ' || u.last_name AS user_name,
-    u.email AS user_email,
-    p.property_id,
-    p.name AS property_name,
-    p.location AS property_location,
-    pay.payment_id,
-    pay.amount AS payment_amount,
-    pay.payment_method
-FROM
-    booking b
-    JOIN users u ON b.user_id = u.user_id
-    JOIN property p ON b.property_id = p.property_id
-    LEFT JOIN payment pay ON b.booking_id = pay.booking_id;
+booking.*,
+ payment.payment_id,
+    payment.amount AS payment_amount,
+    payment.payment_method AS payment_method
+    FROM booking
+
+LEFT JOIN payment payment ON booking.booking_id = payment.booking_id;
